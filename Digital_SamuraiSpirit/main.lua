cf5553fc827527874986b753e1504eeed0bf5b16
Delwyn <suttond22@mymacewan.ca>
2022-03-27 07:30:19 -0600

    Update folder with love game link

--- Digital_SamuraiSpirit/main.lua
+++ Digital_SamuraiSpirit/main.lua
@@ -1,16 +1,31 @@
+
 function love.load()
 
-   -- initialize all decks, selectable characters, gameboard, game pieces. 
-end
- 
-function createRaider()
+   -- initialize all decks, selectable characters, gameboard, game pieces.
+
 end
 
 function love.update(dt)
     -- update on menu changes or selections, performed actions
+    -- exit game
+
+    -- love.event.quit
+
 end
 
 function love.draw()
     -- draw loaded images every update
+    love.window.setFullscreen(true) -- where is the best place to put (load or draw)
+
+    love.graphics.setBackgroundColor(0.68, 0.85, 0.90)
+
 end
 
+function love.keypressed(key)
+  if key == "escape" then
+    love.event.quit()
+  end
+end
+
+function love.mousepressed(x, y, button)
+end
